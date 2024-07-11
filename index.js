f094215c02a2345c1ef497fc56475e5b859db883
AlzubairMohammed <dev.alzubair@gmail.com>
2024-07-11 22:51:06 +0300

    Project config

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
+const URL = process.env.URL;
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`App listen in ${PORT}`));
