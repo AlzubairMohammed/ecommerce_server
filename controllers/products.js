b6658d4a4f8c4926a818a11c1266c36c92f54731
AlzubairMohammed <dev.alzubair@gmail.com>
2024-07-12 01:54:40 +0300

    Add crud logic for products

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,41 @@
-exports.getProducts = async (req, res, next) => {
-  return res.json("Products");
-};
-exports.getProduct = async () => {};
-exports.createProduct = async () => {};
-exports.updateProduct = async () => {};
-exports.deleteProduct = async () => {};
+const asyncWrapper = require("../middlewares/asyncWrapper.js");
+const { models } = require("../database/connection");
+const httpStatus = require("../utils/httpStatus.js");
+const errorResponse = require("../utils/errorResponse");
+const products = require("../models/products.js");
+const { validationResult } = require("express-validator");
+const { products } = models;
+
+exports.getProducts = asyncWrapper(async (req, res) => {
+  let data = await products.findAll();
+  return res.json({ status: httpStatus.SUCCESS, data });
+});
+
+exports.getProduct = asyncWrapper(async (req, res) => {
+  let data = await products.findOne({
+    where: { id: req.params.id },
+  });
+  return res.json({ status: httpStatus.SUCCESS, data });
+});
+
+exports.createProduct = asyncWrapper(async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = errorResponse.create(errors.array(), 400, httpStatus.FAIL);
+    return next(error);
+  }
+  let data = await products.create(req.body);
+  return res.json({ status: httpStatus.SUCCESS, data });
+});
+
+exports.editProduct = asyncWrapper(async (req, res) => {
+  let data = await products.update(req.body, {
+    where: { id: req.params.id },
+  });
+  return res.json({ status: httpStatus.SUCCESS, data });
+});
+
+exports.deleteProduct = asyncWrapper(async (req, res) => {
+  let data = await products.destroy({ where: { id: req.params.id } });
+  return res.json({ status: httpStatus.SUCCESS, data });
+});
