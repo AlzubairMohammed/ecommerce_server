8dd1b1147711dafeb78a31236753dad80a1c605b
AlzubairMohammed <dev.alzubair@gmail.com>
2024-07-11 23:53:57 +0300

    Add some utlis

---
+++ controllers/products.js
@@ -0,0 +1,5 @@
+exports.getProducts = async () => {};
+exports.getProduct = async () => {};
+exports.createProduct = async () => {};
+exports.updateProduct = async () => {};
+exports.deleteProduct = async () => {};
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const httpStatus = require("./utils/httpStatus");
+const error = require("./utils/errorResponse");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const URL = process.env.URL;
 const PORT = process.env.PORT;
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use((error, req, res, next) => {
+  res.status(error.statusCode || 500).json({
+    status: error.statusText || httpStatus.ERROR,
+    message: error.message,
+    code: error.statusCode || 500,
+    data: null,
+  });
+});
 
 app.listen(PORT, () => console.log(`App listen in ${PORT}`));
