fc750e3e1040813ee972c9d129ec65ce964b0071
AlzubairMohammed <dev.alzubair@gmail.com>
2024-07-12 02:41:54 +0300

    Add create product swagger doc

--- routes/products.js
+++ routes/products.js
@@ -10,6 +10,42 @@ const {
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       required:
+ *         - name
+ *         - price
+ *         - quantity
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the product
+ *         name:
+ *           type: string
+ *           description: The product name
+ *         price:
+ *           type: string
+ *           description: The product price
+ *         quantity:
+ *           type: string
+ *           description: The product quantity
+ *       example:
+ *         name: Phone x
+ *         price: 300.00
+ *         quantity: 100
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: The products managing API
+ */
+
+/**
+ * @swagger
  * /products:
  *   get:
  *     summary: Returns the list of all the products
@@ -46,6 +82,28 @@ router.get("/", productsValidator(), getProducts);
  */
 
 router.get("/:id", getProduct);
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: The product was successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       500:
+ *         description: Some server error
+ */
 router.post("/", createProduct);
 router.put("/:id", updateProduct);
 router.delete("/:id", deleteProduct);
